09a7fa159c7e1dfa9049d7b9a951c5b4fb61fc67
yuri <yuri.th03+work@gmail.com>
2023-11-18 21:30:22 +0900

    “ミドルウェアを追加”

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ export default {
   css: ["@/static/css/global.css", "normalize.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [{ src: "~/plugins/firebase.js", mode: "client" }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,

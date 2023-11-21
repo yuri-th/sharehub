22e0ee4277c79c77b78250febe3d4bd2a20ac095
yuri <yuri.th03+work@gmail.com>
2023-11-21 17:37:48 +0900

    vuex実装しました

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,6 +1,18 @@
 import firebase from "~/plugins/firebase";
 
-export default function ({ route, redirect }) {
+export default async function ({ store, route, redirect }) {
+  // Firebase AuthのonAuthStateChangedを使用してVuexストアを更新
+  await new Promise((resolve) => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        store.commit("setUser", user);
+      } else {
+        store.commit("setUser", null);
+      }
+      resolve();
+    });
+  });
+
   const requiresAuth = route.meta.some((meta) => meta.requiresAuth);
   const user = firebase.auth().currentUser;
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
+  buildModules: ["@nuxtjs/vuex"],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [

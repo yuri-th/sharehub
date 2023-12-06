e078b105a82223839dd3ec8d658d9282d4188c67
yuri <yuri.th03+work@gmail.com>
2023-12-06 11:19:46 +0900

    コンソールログ再修正

--- pages/index.vue
+++ pages/index.vue
@@ -233,12 +233,12 @@ export default {
           console.log("likesResponse.data.data:", likesResponse.data.data);
 
           // いいねのデータ内で user_name と tweet_id を比較して存在を確認
-          const existingLike = Object.values(likesResponse.data.data).find(
-            (like) => {
+          const existingLike = Object.entries(likesResponse.data.data).find(
+            ([tweetIdKey, like]) => {
               console.log("like:", like); // デバッグ用のログ
               console.log("like.users:", like.users);
-              console.log("like.tweet_id:", like.tweet_id); // 修正
-              return like.users.includes(user_name) && like.tweet_id == tweetId; // 修正
+              console.log("like.tweet_id:", tweetIdKey); // tweet_id として扱うキー
+              return like.users.includes(user_name) && tweetIdKey == tweetId; // tweet_id として扱うキー
             }
           );
 

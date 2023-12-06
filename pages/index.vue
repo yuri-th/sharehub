b9f54831a20e7c3f53ed90fc2f65ab42ca9c686b
yuri <yuri.th03+work@gmail.com>
2023-12-06 15:12:14 +0900

    ログアウト再変更

--- pages/index.vue
+++ pages/index.vue
@@ -295,14 +295,19 @@ export default {
         .signOut()
         .then(() => {
           alert("ログアウトが完了しました");
-          // ナビゲーション前に同じルートへのナビゲーションを防ぐ
-          this.$router.push("/").catch((err) => {
-            if (err.name !== "NavigationDuplicated") {
-              throw err;
-            }
-          });
           // ログアウト後のリダイレクト
-          this.$router.replace("/");
+          this.$router.push("/").then(
+            () => {
+              ログアウト成功;
+              // 成功時の処理
+            },
+            (err) => {
+              // エラーハンドリング
+              if (err.name !== "NavigationDuplicated") {
+                throw err;
+              }
+            }
+          );
         });
     },
   },

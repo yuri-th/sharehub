e7d36f6b48c43c0c1fb4a9a92e77f56390262d1f
yuri <yuri.th03+work@gmail.com>
2024-07-06 11:55:01 +0900

    change:メインカラー変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,8 +41,6 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    // baseURL: "/",
     baseURL: process.env.API_BASE_URL || 'http://localhost:3000'
   },
 

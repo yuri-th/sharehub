aa276d84125ca990071695a46a1e77a806c5092b
yuri <yuri.th03+work@gmail.com>
2025-09-27 22:41:30 +0900

    firebase.jsの変更

---
+++ plugins/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+import "firebase/auth";
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: process.env.NUXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NUXT_PUBLIC_FIREBASE_APP_ID,
+  })
+}
+
+export default firebase
